6229c5928a40565a49212747642cf644327c1336
RiyaShekha <139801988+RiyaShekha@users.noreply.github.com>
2023-08-01 18:55:41 +0530

    Use Strict

---
+++ 2.3/strictFunction.js
@@ -0,0 +1,11 @@
+"use strict"
+function sum(a,b){
+  var add = a+b;
+ 
+  //if I am not writing the 'var' then it will give the error!! when I am writing this "use strict".
+  
+  console.log(add);
+
+  //it will give the output 30
+}
+sum(10,20);
