309ba75563057759116f08abd67559d75f3d0846
RiyaShekha <139801988+RiyaShekha@users.noreply.github.com>
2023-08-01 21:07:45 +0530

    Data Types in JavaScript

---
+++ 2.5/Data_Types.js
@@ -0,0 +1,28 @@
+// no error
+let message = "hello";
+message = 123456;
+console.log(message); //123456
+
+let n = 123;
+n = 12.345;
+console.log(n); //12.345
+
+//Datatypes in JavaScript:-
+
+console.log(typeof undefined)// "undefined"
+
+console.log(typeof 0) // "number"
+
+console.log(typeof 10n) // "bigint"
+
+console.log(typeof true) // "boolean"
+
+console.log(typeof "foo") // "string"
+
+console.log(typeof Symbol("id")) // "symbol"
+
+console.log(typeof Math) // "object"  
+
+console.log(typeof null) // "object"  
+
+console.log(typeof alert) // "function"  
