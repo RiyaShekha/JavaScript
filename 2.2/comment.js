286c5296a42b792e2a46428218a5d6167df2d3ea
RiyaShekha <139801988+RiyaShekha@users.noreply.github.com>
2023-08-01 18:04:06 +0530

    JavaScript comments and statements

---
+++ 2.2/comment.js
@@ -0,0 +1,7 @@
+// This is one-line comment.
+
+alert('Example of comments');
+
+/* An example with two messages.
+This is a multiline comment.
+*/
---
+++ 2.2/statements.js
@@ -0,0 +1,17 @@
+// statements in javascript are seperated by ';'
+
+console.log("This is a statement");
+
+// it can work without the semicolon in most casses
+// javascript will interpreat line break as semicolon if it does not generate any error
+
+console.log("statement without semicolon")
+
+// if interpreting the line break as semicolon does not from an valid statement than javascript will not do that
+// Example
+console.log(
+    50+
+    5
+);
+
+// not adding semicolon can cause errors in many cases!
